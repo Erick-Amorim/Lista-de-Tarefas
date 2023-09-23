1b62497af1f7622f7f26b021dadbd9619e30dbfd
Erick-Amorim <erick7amorim@gmail.com>
2023-09-23 17:56:02 -0300

    projeto refatorado

--- src/App.js
+++ src/App.js
@@ -1,85 +1,46 @@
-import React, { useState } from 'react';
-import { MdDelete } from 'react-icons/md';
+import React, { useState } from "react";
 
-import './App.css';
+import NewTodo from "./components/NewTodo";
+import TodoList from "./components/TodoList";
+
 
 const App = () => {
-  const ESCAPE_KEY = 27;
-  const ENTER_KEY = 13;
-
   const [todos, setTodos] = useState([]);
-  const [value, setValue] = useState('');
 
-  const erase = () => {
-    setValue('');
-  }
-
-  const submit = () => {
-    console.log(value);
-
+  const onNewTodo = (value) => {
     setTodos([
       ...todos,
-      { id: new Date().getTime(), title: value, checked: false },
-
+      {
+        id: new Date().getTime(),
+        title: value,
+        checked: false,
+      },
     ]);
-
-    erase();
-  }
-
-  const onKeyDown = (event) => {
-    if (event.which === ENTER_KEY) {
-      submit();
-    } else if (event.which === ESCAPE_KEY) {
-      erase();
-    }
-  }
-
-  const onChange = (event) => {
-    setValue(event.target.value);
-  }
+  };
 
   const onToggle = (todo) => {
-    console.log('toggle', todo);
     setTodos(
-      todos.map((obj) => obj.id === todo.id ? { ...obj, checked: !todo.checked } : obj
-  ));
+      todos.map((obj) =>
+        obj.id === todo.id ? { ...obj, checked: !todo.checked } : obj
+      )
+    );
   };
 
   const onRemove = (todo) => {
     setTodos(todos.filter((obj) => obj.id !== todo.id));
-  }
+  };
 
   return (
     <section id="app" className="container">
       <header>
-        <h1 className='title'>Todo</h1>
+        <h1 className="title">todo</h1>
       </header>
-      <section className='main'>
-        <input className='new-todo' placeholder='O que precisa ser feito?'
-        value = {value}
-        onChange={onChange}
-        onKeyDown={onKeyDown}/>
-        <ul className='todo-list'>
-          {
-            todos.map((todo) => (
-              <li key={todo.id.toString()}>
-                <span
-                  className={["todo", todo.checked ? "checked" : ""].join(" ")}
-                  onClick={() => onToggle(todo)}
-                  role='button'
-                  tabIndex={0}>
-                  {todo.title}</span>
-                <button className='remove' type='button'
-                onClick={() => onRemove(todo)}>
-                  <MdDelete size={28} />
-                </button>
-              </li>
-            ))
-          }
-        </ul>
+      <section className="main">
+        <NewTodo onNewTodo={onNewTodo} />
+        <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
       </section>
-    </ section>
+    </section>
   );
-}
+};
 
 export default App;
